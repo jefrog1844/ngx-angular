81a189cbd456d20142a8a1d9b3932a91e5c9263a
jefrog1844 <jefrog1844@gmail.com>
2024-03-17 14:04:28 -0500

    implement unit tests

--- projects/ngx-angular/src/lib/checkbox/checkbox.component.spec.ts
+++ projects/ngx-angular/src/lib/checkbox/checkbox.component.spec.ts
@@ -1,7 +1,95 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 
-import { FormControl, NgControl } from '@angular/forms';
+import { Component, DebugElement } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  NgControl,
+  NgModel,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { CheckboxComponent } from './checkbox.component';
+
+@Component({
+  template: `
+    <form class="mui-form" [formGroup]="singleCheckForm">
+      <lib-checkbox
+        id="single-check"
+        label="Single checkbox"
+        value="single"
+        formControlName="singleCheck"
+      ></lib-checkbox>
+    </form>
+  `,
+})
+class SingleCheckboxComponent {
+  singleCheckForm = new FormGroup({
+    singleCheck: new FormControl(false),
+  });
+}
+
+@Component({
+  template: `
+    <form class="mui-form" [formGroup]="multiCheckForm">
+      <lib-checkbox
+        id="multi-1"
+        label="Option 1"
+        value="one"
+        formControlName="oneCheck"
+      ></lib-checkbox>
+      <lib-checkbox
+        id="multi-2"
+        label="Option 2"
+        value="two"
+        formControlName="twoCheck"
+      ></lib-checkbox>
+    </form>
+  `,
+})
+class MultipleCheckboxComponent {
+  multiCheckForm = new FormGroup({
+    oneCheck: new FormControl(false),
+    twoCheck: new FormControl(false),
+  });
+}
+
+@Component({
+  template: `
+    <form class="mui-form" #c="ngForm">
+      <lib-checkbox
+        id="ngModel-check"
+        label="NgModel Check"
+        value="one"
+        [(ngModel)]="checkValue"
+        name="checkValue"
+      ></lib-checkbox>
+    </form>
+  `,
+})
+class NgModelCheckboxComponent {
+  checkValue: any = false;
+}
+
+@Component({
+  template: `
+    <lib-checkbox
+      id="formControl-check"
+      label="Standalone checkbox"
+      value="alone"
+      [formControl]="formControlCheck"
+    ></lib-checkbox>
+  `,
+})
+class FormControlCheckboxComponent {
+  formControlCheck: FormControl = new FormControl(false);
+}
 
 describe('CheckboxComponent', () => {
   let component: CheckboxComponent;
@@ -17,7 +105,13 @@ describe('CheckboxComponent', () => {
     };
 
     await TestBed.configureTestingModule({
-      imports: [CheckboxComponent],
+      imports: [CheckboxComponent, ReactiveFormsModule, FormsModule],
+      declarations: [
+        SingleCheckboxComponent,
+        MultipleCheckboxComponent,
+        NgModelCheckboxComponent,
+        FormControlCheckboxComponent,
+      ],
     })
       .overrideComponent(CheckboxComponent, {
         add: { providers: [NG_CONTROL_PROVIDER] },
@@ -31,5 +125,240 @@ describe('CheckboxComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  describe('Single checkbox', () => {
+    let fixture: ComponentFixture<SingleCheckboxComponent>;
+    let testComponent: SingleCheckboxComponent;
+    let checkboxDebugElement: DebugElement;
+    let checkboxNativeElement: HTMLElement;
+    let checkboxInstance: CheckboxComponent;
+    let checkBoxInputElement: HTMLInputElement;
+
+    beforeEach(async () => {
+      fixture = TestBed.createComponent(SingleCheckboxComponent);
+      fixture.detectChanges();
+
+      testComponent = fixture.debugElement.componentInstance;
+
+      checkboxDebugElement = fixture.debugElement.query(
+        By.directive(CheckboxComponent)
+      );
+      checkboxInstance =
+        checkboxDebugElement.injector.get<CheckboxComponent>(CheckboxComponent);
+      checkboxNativeElement = checkboxDebugElement.nativeElement;
+      checkBoxInputElement = checkboxDebugElement.query(
+        By.css('input')
+      ).nativeElement;
+    });
+
+    it('should add and remove the checked state', () => {
+      expect(checkBoxInputElement.checked).toBe(false);
+
+      testComponent.singleCheckForm.controls.singleCheck.setValue(true);
+      fixture.detectChanges();
+
+      expect(checkBoxInputElement.checked).toBe(true);
+
+      testComponent.singleCheckForm.controls.singleCheck.setValue(false);
+      fixture.detectChanges();
+
+      expect(checkBoxInputElement.checked).toBe(false);
+    });
+
+    it('should toggle checked state on click', () => {
+      expect(checkBoxInputElement.checked).toBe(false);
+
+      checkBoxInputElement.click();
+      fixture.detectChanges();
+
+      expect(checkBoxInputElement.checked).toBe(true);
+
+      checkBoxInputElement.click();
+      fixture.detectChanges();
+
+      expect(checkBoxInputElement.checked).toBe(false);
+    });
+
+    it('should not toggle `checked` state upon interation while disabled', () => {
+      testComponent.singleCheckForm.controls.singleCheck.disable();
+      fixture.detectChanges();
+
+      checkBoxInputElement.click();
+      fixture.detectChanges();
+
+      expect(checkBoxInputElement.checked).toBe(false);
+    });
+
+    it('should copy the user-provided id', () => {
+      expect(checkboxNativeElement.id).toBeFalsy();
+
+      expect(checkBoxInputElement.id).toBe('single-check');
+    });
+  });
+
+  describe('with multiple checkboxes', () => {
+    let fixture: ComponentFixture<MultipleCheckboxComponent>;
+    beforeEach(() => {
+      fixture = TestBed.createComponent(MultipleCheckboxComponent);
+      fixture.detectChanges();
+    });
+
+    it('should assign a unique id to each checkbox', () => {
+      const [firstId, secondId] = fixture.debugElement
+        .queryAll(By.directive(CheckboxComponent))
+        .map(
+          (debugElement) => debugElement.nativeElement.querySelector('input').id
+        );
+
+      expect(firstId).toEqual('multi-1');
+      expect(secondId).toEqual('multi-2');
+      expect(firstId).not.toEqual(secondId);
+    });
+  });
+
+  describe('NgModel checkbox', () => {
+    let fixture: ComponentFixture<NgModelCheckboxComponent>;
+    let testComponent: NgModelCheckboxComponent;
+    let checkboxDebugElement: DebugElement;
+    let checkboxNativeElement: HTMLElement;
+    let checkboxInstance: CheckboxComponent;
+    let checkBoxInputElement: HTMLInputElement;
+    let ngModel: NgModel;
+
+    beforeEach(async () => {
+      fixture = TestBed.createComponent(NgModelCheckboxComponent);
+      fixture.detectChanges();
+
+      testComponent = fixture.debugElement.componentInstance;
+
+      checkboxDebugElement = fixture.debugElement.query(
+        By.directive(CheckboxComponent)
+      );
+      checkboxInstance =
+        checkboxDebugElement.injector.get<CheckboxComponent>(CheckboxComponent);
+      checkboxNativeElement = checkboxDebugElement.nativeElement;
+      checkBoxInputElement = checkboxDebugElement.query(
+        By.css('input')
+      ).nativeElement;
+      ngModel = checkboxDebugElement.injector.get<NgModel>(NgModel);
+    });
+
+    it('should add and remove the checked state', fakeAsync(() => {
+      expect(checkBoxInputElement.checked).toBe(false);
+
+      testComponent.checkValue = true;
+      fixture.detectChanges();
+      tick();
+      expect(checkBoxInputElement.checked).toBe(true);
+
+      testComponent.checkValue = false;
+      fixture.detectChanges();
+      tick();
+
+      expect(checkBoxInputElement.checked).toBe(false);
+    }));
+
+    it('should toggle checked state on click', fakeAsync(() => {
+      expect(checkBoxInputElement.checked).toBe(false);
+
+      checkBoxInputElement.click();
+      fixture.detectChanges();
+      tick();
+
+      expect(checkBoxInputElement.checked).toBe(true);
+
+      checkBoxInputElement.click();
+      fixture.detectChanges();
+      tick();
+
+      expect(checkBoxInputElement.checked).toBe(false);
+    }));
+
+    it('should copy the user-provided id', () => {
+      expect(checkboxNativeElement.id).toBeFalsy();
+
+      expect(checkBoxInputElement.id).toBe('ngModel-check');
+    });
+
+    it('should not toggle `checked` state upon interation while disabled', fakeAsync(() => {
+      checkboxInstance.ngControl.control.disable();
+      fixture.detectChanges();
+      tick();
+
+      checkBoxInputElement.click();
+      fixture.detectChanges();
+
+      expect(checkBoxInputElement.checked).toBe(false);
+    }));
+  });
+
+  describe('FormControl checkbox', () => {
+    let fixture: ComponentFixture<FormControlCheckboxComponent>;
+    let testComponent: FormControlCheckboxComponent;
+    let checkboxDebugElement: DebugElement;
+    let checkboxNativeElement: HTMLElement;
+    let checkboxInstance: CheckboxComponent;
+    let checkBoxInputElement: HTMLInputElement;
+
+    beforeEach(async () => {
+      fixture = TestBed.createComponent(FormControlCheckboxComponent);
+      fixture.detectChanges();
+
+      testComponent = fixture.debugElement.componentInstance;
+
+      checkboxDebugElement = fixture.debugElement.query(
+        By.directive(CheckboxComponent)
+      );
+      checkboxInstance =
+        checkboxDebugElement.injector.get<CheckboxComponent>(CheckboxComponent);
+      checkboxNativeElement = checkboxDebugElement.nativeElement;
+      checkBoxInputElement = checkboxDebugElement.query(
+        By.css('input')
+      ).nativeElement;
+    });
+
+    it('should add and remove the checked state', () => {
+      expect(checkBoxInputElement.checked).toBe(false);
+
+      testComponent.formControlCheck.setValue(true);
+      fixture.detectChanges();
+
+      expect(checkBoxInputElement.checked).toBe(true);
+
+      testComponent.formControlCheck.setValue(false);
+      fixture.detectChanges();
+
+      expect(checkBoxInputElement.checked).toBe(false);
+    });
+
+    it('should toggle checked state on click', () => {
+      expect(checkBoxInputElement.checked).toBe(false);
+
+      checkBoxInputElement.click();
+      fixture.detectChanges();
+
+      expect(checkBoxInputElement.checked).toBe(true);
+
+      checkBoxInputElement.click();
+      fixture.detectChanges();
+
+      expect(checkBoxInputElement.checked).toBe(false);
+    });
+
+    it('should not toggle `checked` state upon interation while disabled', () => {
+      testComponent.formControlCheck.disable();
+      fixture.detectChanges();
+
+      checkBoxInputElement.click();
+      fixture.detectChanges();
+
+      expect(checkBoxInputElement.checked).toBe(false);
+    });
+
+    it('should copy the user-provided id', () => {
+      expect(checkboxNativeElement.id).toBeFalsy();
+
+      expect(checkBoxInputElement.id).toBe('formControl-check');
+    });
   });
 });
