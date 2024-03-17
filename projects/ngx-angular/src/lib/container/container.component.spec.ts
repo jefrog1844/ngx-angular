c9f15c7ecee8bb784eb57763445b0ff8398d0d63
jefrog1844 <jefrog1844@gmail.com>
2024-03-17 14:24:57 -0500

    add unit tests

--- projects/ngx-angular/src/lib/container/container.component.spec.ts
+++ projects/ngx-angular/src/lib/container/container.component.spec.ts
@@ -5,19 +5,31 @@ import { ContainerComponent } from './container.component';
 describe('ContainerComponent', () => {
   let component: ContainerComponent;
   let fixture: ComponentFixture<ContainerComponent>;
+  let containerEl: HTMLDivElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ContainerComponent]
-    })
-    .compileComponents();
-    
+      imports: [ContainerComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ContainerComponent);
     component = fixture.componentInstance;
+    containerEl = fixture.nativeElement.querySelector('div');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('container should have class mui-container', () => {
+    expect(containerEl).toHaveClass('mui-container');
+  });
+
+  it('container should have class mui-container-fluid', () => {
+    component.fluid = '';
+    component.ngAfterViewInit();
+    fixture.detectChanges();
+    expect(containerEl).toHaveClass('mui-container-fluid');
   });
 });
