11b07e1226936483bdc6e3ad887374d7d39e92a8
jefrog1844 <jefrog1844@gmail.com>
2024-03-17 22:47:14 -0500

    implement unit tests

--- projects/ngx-angular/src/lib/panel/panel.component.spec.ts
+++ projects/ngx-angular/src/lib/panel/panel.component.spec.ts
@@ -5,19 +5,24 @@ import { PanelComponent } from './panel.component';
 describe('PanelComponent', () => {
   let component: PanelComponent;
   let fixture: ComponentFixture<PanelComponent>;
+  let panelEl: HTMLDivElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PanelComponent]
-    })
-    .compileComponents();
-    
+      imports: [PanelComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(PanelComponent);
     component = fixture.componentInstance;
+    panelEl = fixture.nativeElement.querySelector('div');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('panel should have class mui-panel', () => {
+    expect(panelEl).toHaveClass('mui-panel');
   });
 });
