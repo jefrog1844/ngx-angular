cbef3a46baa95921c7a22f2ead8ffe1ad453bc14
jefrog1844 <jefrog1844@gmail.com>
2024-03-17 23:02:18 -0500

    add unit tests

--- projects/ngx-angular/src/lib/row/row.component.spec.ts
+++ projects/ngx-angular/src/lib/row/row.component.spec.ts
@@ -5,19 +5,24 @@ import { RowComponent } from './row.component';
 describe('RowComponent', () => {
   let component: RowComponent;
   let fixture: ComponentFixture<RowComponent>;
+  let rowEl = HTMLDivElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RowComponent]
-    })
-    .compileComponents();
-    
+      imports: [RowComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(RowComponent);
     component = fixture.componentInstance;
+    rowEl = fixture.nativeElement.querySelector('div');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('row should have class mui-row', () => {
+    expect(rowEl).toHaveClass('mui-row');
   });
 });
