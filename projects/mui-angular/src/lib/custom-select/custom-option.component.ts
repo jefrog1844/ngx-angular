832481c3fc98d7c5e7c7cd95811e70e8ca0daddc
Jeffrey Rogers <rogersj3@magellanhealth.com>
2024-03-20 17:55:00 -0500

    add custom select component

---
+++ projects/mui-angular/src/lib/custom-select/custom-option.component.ts
@@ -0,0 +1,46 @@
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  ElementRef,
+  Input,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
+
+@Component({
+  selector: 'mui-custom-option',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <ng-template #templateref>
+      <option
+        #option
+        [disabled]="disabled"
+        [value]="value"
+        [ngClass]="{
+          'mui--text-black': value !== '',
+          'mui--text-placeholder': value === ''
+        }"
+      >
+        {{ label }}
+      </option>
+    </ng-template>
+  `,
+  styles: [],
+})
+export class CustomOptionComponent {
+  @Input() disabled?: boolean = false;
+
+  @Input() hidden: boolean = false;
+
+  @Input() label: string;
+
+  @Input() value: string;
+
+  @ViewChild('templateref', { static: true })
+  public optionTemplate: TemplateRef<HTMLOptionElement>;
+
+  @ViewChild('option', { static: true, read: ElementRef }) option: ElementRef;
+
+  constructor() {}
+}
---
+++ projects/mui-angular/src/lib/custom-select/custom-select.component.ts
@@ -0,0 +1,148 @@
+import { CommonModule } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  ContentChildren,
+  ElementRef,
+  Input,
+  QueryList,
+  Renderer2,
+  Self,
+  ViewChild,
+} from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
+import { CustomOptionComponent } from './custom-option.component';
+
+@Component({
+  selector: 'mui-custom-select',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <div class="mui-select">
+      <select
+        id="id"
+        #select
+        (change)="onChange($event.target.value)"
+        (blur)="onTouched()"
+        [disabled]="disabled"
+        [required]="required"
+        [ngClass]="{ 'mui--text-placeholder': ngControl.value === '' }"
+      ></select>
+      <label [for]="id" tabindex="-1">{{ label }}</label>
+      <div class="mui-select__menu" #menu>
+        <ng-container *ngFor="let item of items">
+          <ng-container [ngTemplateOutlet]="item.itemTemplate"></ng-container>
+        </ng-container>
+      </div>
+    </div>
+  `,
+})
+export class CustomSelectComponent
+  implements AfterViewInit, ControlValueAccessor
+{
+  @Input() autofocus?: boolean = false;
+
+  @Input() disabled?: boolean = false;
+
+  @Input() id?: string;
+
+  @Input() label?: string;
+
+  @Input() required?: boolean = false;
+
+  @ContentChildren(CustomOptionComponent)
+  items!: QueryList<CustomOptionComponent>;
+
+  @ViewChild('select', { static: true, read: ElementRef }) select: ElementRef;
+
+  constructor(
+    @Self() public ngControl: NgControl,
+    private renderer: Renderer2,
+    private wrapper: ElementRef
+  ) {
+    ngControl.valueAccessor = this;
+  }
+
+  ngAfterViewInit(): void {
+    // cache references to select and wrapper
+    const selectEl: HTMLInputElement = this.select.nativeElement;
+    const wrapperEl: HTMLElement = this.wrapper.nativeElement;
+
+    // autofocus
+    if (this.autofocus) {
+      this.renderer.selectRootElement(selectEl).focus();
+    }
+
+    /**
+     * name - gets set on NgControl through inputs for NgModel and formControlName directives only.
+     * Does not work for standalone FormControl directive
+     */
+    if (this.ngControl.name) {
+      this.renderer.setAttribute(
+        selectEl,
+        'name',
+        this.ngControl.name.toString()
+      );
+    } else {
+      console.warn(`
+       It looks like you're using formControl which does not have an input for the
+       name attribute.  If the name attribute is required (i.e. when submitting a form),
+       it is recommended to use either ngModel or formControlName.`);
+    }
+
+    // set attributes
+    ['id', 'placeholder', 'required'].forEach((attrName) => {
+      const attrVal = this[attrName];
+      if (attrVal) {
+        this.renderer.setAttribute(selectEl, attrName, attrVal);
+      }
+    });
+
+    // remove attributes from wrapper
+    this.renderer.removeAttribute(wrapperEl, 'id');
+
+    // set the selected index
+    const index = this.items
+      .toArray()
+      .findIndex((option) => option.value === this.ngControl.value);
+    this.renderer.setProperty(
+      this.select.nativeElement,
+      'selectedIndex',
+      index
+    );
+  }
+
+  /**
+   * This code listens for updates from the component and
+   * writes them to the native <select> element
+   *
+   * NOTE: writeValue gets called before ngOnInit Angular issue #29218
+   * Side effect of the issue is that trying to access the selectEl
+   * cached in ngOnInit will result in undefined error in writeValue.
+   * This also requires ViewChild({static: true}) in order for select to
+   * be available before onChange is called.
+   * @param value - value from <mui-select>
+   */
+  writeValue(value: string): void {
+    // update select field with value received from outer component
+    this.renderer.setProperty(this.select.nativeElement, 'value', value);
+  }
+
+  /**
+   * Code below this point is all boilerplate - DO NOT CHANGE
+   */
+  onTouched: () => void;
+  onChange: (value: string) => void;
+
+  registerOnChange(fn: () => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(disabled: boolean): void {
+    this.disabled = disabled;
+  }
+}
