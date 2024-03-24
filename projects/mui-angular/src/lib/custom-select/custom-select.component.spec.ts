2176026bd5fe8ce88a7d59d3a25934ad617d8719
jefrog1844 <jefrog1844@gmail.com>
2024-03-24 13:03:37 -0500

    add provider for control value accessor

--- projects/mui-angular/src/lib/custom-select/custom-select.component.spec.ts
+++ projects/mui-angular/src/lib/custom-select/custom-select.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { FormControl, NgControl } from '@angular/forms';
 import { CustomSelectComponent } from './custom-select.component';
 
 describe('CustomSelectComponent', () => {
@@ -7,11 +8,22 @@ describe('CustomSelectComponent', () => {
   let fixture: ComponentFixture<CustomSelectComponent>;
 
   beforeEach(async () => {
+    const NG_CONTROL_PROVIDER = {
+      provide: NgControl,
+      useClass: class extends NgControl {
+        control = new FormControl();
+        viewToModelUpdate(): void {}
+      },
+    };
+
     await TestBed.configureTestingModule({
-      imports: [CustomSelectComponent]
+      imports: [CustomSelectComponent],
     })
-    .compileComponents();
-    
+      .overrideComponent(CustomSelectComponent, {
+        add: { providers: [NG_CONTROL_PROVIDER] },
+      })
+      .compileComponents();
+
     fixture = TestBed.createComponent(CustomSelectComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
