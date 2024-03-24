f038160d3215c86a90c443779bf81cb34755031d
jefrog1844 <jefrog1844@gmail.com>
2024-03-24 11:16:56 -0500

    add depth input to appbar

--- projects/mui-angular/src/lib/appbar/appbar.component.ts
+++ projects/mui-angular/src/lib/appbar/appbar.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'mui-appbar',
   standalone: true,
   imports: [],
-  template: `<div class="mui-appbar mui--z3"><ng-content></ng-content></div>`,
+  template: `<div #appbar class="mui-appbar">
+    <ng-content></ng-content>
+  </div>`,
 })
-export class AppbarComponent {}
+export class AppbarComponent implements AfterViewInit {
+  @Input() depth?: string;
+
+  @ViewChild('appbar')
+  appbar!: ElementRef<HTMLDivElement>;
+
+  private appbarEl!: HTMLDivElement;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    if (this.depth) {
+      this.appbarEl = this.appbar.nativeElement;
+      this.renderer.addClass(this.appbarEl, 'mui--' + this.depth);
+    }
+  }
+}
